0ba065a116214bc0287271886e58400def27faa9
Vlad <88582918+ikbenvlad@users.noreply.github.com>
2025-05-19 11:12:43 +0200

    enum aangemaakt
    Enum met commands aangemaakt

--- textBaseAdventure/TextBasedAdventure/TextBasedAdventure/Command.cs
+++ textBaseAdventure/TextBasedAdventure/TextBasedAdventure/Command.cs
@@ -10,15 +10,26 @@ namespace TextBasedAdventure
     {
         public string CommandInfo { get; set; }
         public string CommandName { get; set; }
-        public int Accessor { get; set; }
+        public CommandType Accessor { get; set; }
         public Action ExecuteCommand { get; set; }
 
         public Command(string commandInfo, string commandName, int accessor, Action executeCommand)
         {
             CommandInfo = commandInfo;
             CommandName = commandName;
-            Accessor = accessor;
+            Accessor = (CommandType)accessor;
             ExecuteCommand = executeCommand;
+        }
+
+        public enum CommandType
+        {
+            Move,
+            Attack,
+            UseItem,
+            Talk,
+            Examine,
+            Inventory,
+            Quit
         }
     }
 }
