0dd96400602854d02d8ace9aa3bab2312cd4bad4
Vlad <88582918+ikbenvlad@users.noreply.github.com>
2025-06-18 18:05:28 +0200

    Lm ben alles kwijt
    kkr

--- textBaseAdventure/TextBasedAdventure/TextBasedAdventure/Program.cs
+++ textBaseAdventure/TextBasedAdventure/TextBasedAdventure/Program.cs
@@ -6,11 +6,32 @@ using System.Threading.Tasks;
 
 namespace TextBasedAdventure
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Command test = new Command("void");
+            Console.WriteLine("Please enter your name...");
+            string name = Console.ReadLine()?.ToLower();
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "child";
+                return;
+            }
+
+            Console.WriteLine($"Welcome, {name}.");
+            Thread.Sleep(1000);
+            Console.WriteLine("Today was a long day, try to get some rest.");
+            Thread.Sleep(1000);
+            Console.WriteLine("You go to sleep and fade off to sleep.");
+            Thread.Sleep(4000);
+            Console.WriteLine(".");
+            Thread.Sleep(1000);
+            Console.WriteLine(".");
+            Thread.Sleep(1000);
+            Console.WriteLine(".");
+            Thread.Sleep(4000);
+            Console.Clear();
+            WakingUp.WokeUp();
         }
     }
 }
